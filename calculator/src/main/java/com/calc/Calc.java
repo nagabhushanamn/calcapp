7c0d0317afbadf8453d51dfa8108fc15a08926e7
nag <nagabhushanamn@gmail.com>
2016-08-11 13:17:09 +0530

    mul func

--- calculator/src/main/java/com/calc/Calc.java
+++ calculator/src/main/java/com/calc/Calc.java
@@ -6,22 +6,20 @@ public class Calc {
 		double result = a + b;
 		return result;
 	}
-	
+
 	public double sub(double a, double b) {
 		double result = a - b;
 		return result;
 	}
-	
+
 	public double mul(double a, double b) {
 		double result = a * b;
 		return result;
 	}
-	
 
 	public double div(double a, double b) {
 		double result = a / b;
 		return result;
 	}
-	
-	
+
 }
